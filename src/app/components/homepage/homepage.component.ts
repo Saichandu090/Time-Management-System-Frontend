cb5f984b96b87ef8d97ae0f96fe920276c1bc4f2
Saichandu090 <marrisaichandu143@gmail.com>
2024-12-31 12:14:32 +0530

    Added timer with all functionalities to homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,10 +7,79 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+
+  noOfBreaks: number = 0;
+
+  breakTime: number = 0;
 
   ms: any = '0' + 0;
   sec: any = '0' + 0;
   min: any = '0' + 0;
   hr: any = '0' + 0;
+
+  startTimer: any;
+  running: boolean = false;
+  pauseStart: Date = new Date();
+
+  breakStart: number = 0;
+  time='';
+
+  start(): void {
+    if (!this.running) {
+      this.running = true;
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+        if (this.ms === 100) {
+          this.sec++;
+          this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+          this.ms = '0' + 0;
+        }
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+
+        if (this.min === 60) {
+          this.hr++;
+          this.hr = this.hr < 10 ? '0' + this.hr : this.hr;
+          this.min = '0' + 0;
+        }
+      }, 10);
+    } else {
+      this.stop();
+    }
+  } // start() end
+
+
+  stop(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+    this.noOfBreaks++;
+    this.breakTime = JSON.parse(this.sec) + this.breakTime;
+  } // stop() end
+
+  reset(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+    this.hr = this.min = this.sec = this.ms = '0' + 0;
+    this.noOfBreaks = 0;
+  } // reset() end
+
+  ngOnInit(): void {
+    this.breakStart = this.pauseStart.getTime();
+
+    const date=new Date(this.breakStart);
+    const hours = date.getHours(); // Get the hours
+    const minutes = date.getMinutes(); // Get the minutes
+    const seconds = date.getSeconds(); // Get the seconds
+
+    // Format the time in HH:mm:ss format
+    this.time= `${hours}:${minutes}:${seconds}`;
+  }
+
 }
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,47 +1,60 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { IJsonResponse, ILoggedInUser } from '../../models/interfaces/response';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,MatSidenavModule, MatButtonModule,MatMenuModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
 
-  userService: UserService=inject(UserService);
+  userService: UserService = inject(UserService);
 
-  snackBar:MatSnackBar=inject(MatSnackBar);
+  snackBar: MatSnackBar = inject(MatSnackBar);
 
-  router:Router=inject(Router);
+  router: Router = inject(Router);
 
-  loggedInUser:ILoggedInUser={
-    email:'',
-    role:'',
-    loginId:0
+  showFiller = false;
+
+  loggedInUser: ILoggedInUser = {
+    email: '',
+    role: '',
+    loginId: 0
   }
 
-  onLogOut(){
-    const rs=confirm("Do you want to logout?");
-    if(rs){
-      const currentUser=localStorage.getItem("CurrentUser");
-      if(currentUser){
-        this.loggedInUser=JSON.parse(currentUser);
-      }
-      this.userService.onLogOut(this.loggedInUser.loginId,this.loggedInUser).subscribe({
-        next:(res:IJsonResponse)=>{
-          this.snackBar.open(res.message,'',{duration:3000});
+  updateCurrentUser(): void{
+    const currentUser = localStorage.getItem("CurrentUser");
+    if (currentUser) {
+      this.loggedInUser = JSON.parse(currentUser);
+    }
+  }
+
+  onLogOut() {
+    const rs = confirm("Do you want to logout?");
+    if (rs) {
+      this.userService.onLogOut(this.loggedInUser.loginId, this.loggedInUser).subscribe({
+        next: (res: IJsonResponse) => {
+          this.snackBar.open(res.message, '', { duration: 3000 });
           localStorage.clear();
           this.router.navigateByUrl("/login");
         },
-        error:(res:IJsonResponse)=>{
-          this.snackBar.open(res.message,'',{duration:3000})
+        error: (res: IJsonResponse) => {
+          this.snackBar.open(res.message, '', { duration: 3000 })
         }
       })
     }
   }//onLogOut() end
+
+  ngOnInit(): void {
+    this.updateCurrentUser();
+  }
 }
